239e157a05cbcc02a6d0f3375929f84275860020
ArcueidShiki <52120441+ArcueidShiki@users.noreply.github.com>
2024-09-01 16:56:05 +0800

    Add dashboard to locales/resources list
    Signed-off-by: ArcueidShiki <52120441+ArcueidShiki@users.noreply.github.com>

--- src/app/(main)/dashboard/features/AgentList.tsx
+++ src/app/(main)/dashboard/features/AgentList.tsx
@@ -2,30 +2,26 @@
 
 import { Grid } from '@lobehub/ui';
 import { memo } from 'react';
-// import { useTranslation } from 'react-i18next';
 import { Flexbox } from 'react-layout-kit';
 
 import AgentCard from './AgentCard';
+import { useTranslation } from 'react-i18next';
 
 const AgentList = memo(() => {
-  // const { t } = useTranslation('dashboard');
+  const { t } = useTranslation('dashboard');
 
   return (
     <Flexbox direction="vertical" gap={16}>
-      {/* Section for Summary */}
-      {/* <h2>{t('title.summary')}</h2> */}
-      <h2>Summary</h2>
-      <Grid rows={2}>
+      <h2>{t(`title.summary`)}</h2>
+      <Grid rows={3}>
         {/* <AgentCard chartType="EChart" title={t('title.summary')} /> */}
         <AgentCard chartType="EChart"/>
         <AgentCard chartType="LineChart"/>
         <AgentCard chartType="LineChart"/>
       </Grid>
 
-      {/* Section for Activity */}
-      {/* <h2>{t('title.activity')}</h2> */}
-      <h2>Activity</h2>
-      <Grid rows={2}>
+      <h2>{t("title.activity")}</h2>
+      <Grid rows={3}>
         <AgentCard chartType="LineChart"/>
         <AgentCard chartType="LineChart"/>
         <AgentCard chartType="LineChart"/>
